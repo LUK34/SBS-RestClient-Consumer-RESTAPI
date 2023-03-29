cc49bf8437768b8664fdf865519779ad441f81f4
luke rajan mathew <lukerajan.mathew@gmail.com>
2023-03-29 19:32:57 +0300

    first commit

---
+++ src/main/java/kw/kng/app/SbsRestClientConsumerRestApiApplication.java
@@ -0,0 +1,13 @@
+package kw.kng.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbsRestClientConsumerRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbsRestClientConsumerRestApiApplication.class, args);
+	}
+
+}
